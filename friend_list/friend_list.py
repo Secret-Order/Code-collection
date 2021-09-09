e9f477463dbb7571af94f870e6df6bc2a266e1d7
Godiez1 <Godjangg@gmail.com>
2021-09-09 15:11:31 +0700

    Update #1

--- friend_list/friend_list.py
+++ friend_list/friend_list.py
@@ -1,27 +1,34 @@
 import random, sys, doctest
 
-def read_file(filename):
-    lines = open(filename, encoding="utf8").read().splitlines()
-    friend_list = [i.split() for i in lines]
-    return friend_list
+class SKE:
+    def __init__(self, filename):
+        self.friend_table = self.read_friend(filename)
+        self.nickname_table = self.extract_nickname_list(self.friend_table)
+        self.id_table = self.extract_id_lists(self.friend_table)
 
-def extract_nickname_lists(friend_table):
-    nickname_table = []
-    for i in range(len(friend_table)):
-        nickname_table.append(friend_table[i][3])
-    return nickname_table
 
-def extract_id_lists(friend_table):
-    id_table = []
-    for i in range(len(friend_table)):
-        id_table.append(friend_table[i][0])
-    return id_table
+
+    @staticmethod
+    def read_friend(filename):
+        lines = open(filename, encoding="utf8").read().splitlines()
+        friend_list = [i.split() for i in lines]
+        return friend_list
+
+
+    def extract_nickname_list(self, friend_table):
+        return [friend_table[i][3] for i in range(len(friend_table))]
+
+    def extract_id_lists(self, friend_table):
+        return [friend_table[i][0] for i in range(len(friend_table))]
+
+
 
 def read_nickname(member):
     list_nickname = []
     for i in range(member):
         list_nickname.append(input(f"Enter Nickname member{i + 1}: "))
     return list_nickname
+
 
 def find_index(table, list_input):
     list_nickname_index = []
@@ -31,6 +38,7 @@ def find_index(table, list_input):
                 list_nickname_index.append(table.index(j))
                 break
     return sorted(list_nickname_index)
+
 
 def random_member(friend_table, list_nickname_index, member):
     random_time = int(input("How many people: "))
@@ -45,9 +53,11 @@ def random_member(friend_table, list_nickname_index, member):
     for i in range(len(random_member_index)):
         print(f"{i + 1}:", *friend_table[list_nickname_index[random_member_index[i]]])
 
+
 def display_members(friend_table, list_nickname_index):
     for i in list_nickname_index:
         print(*friend_table[i])
+
 
 def group_members(friend_table, nickname_table):
     member = int(input("How Many people in a Group: "))
@@ -57,6 +67,7 @@ def group_members(friend_table, nickname_table):
     display_members(friend_table, list_nickname_index)
     if input("Random members? y/n: ").lower() == "y":
         random_member(friend_table, list_nickname_index, member)
+
 
 def operate(friend_table, nickname_table, id_table):
     print("1. Group")
@@ -76,15 +87,22 @@ def operate(friend_table, nickname_table, id_table):
             id_student = [input("Enter ID: ")]
             display_members(friend_table, find_index(id_table, id_student))
 
+
 def main():
+
     filename = "friend_list.txt"
-    friend_table = read_file(filename)
-    # print(friend_table)
-    nickname_table = extract_nickname_lists(friend_table)
-    id_table = extract_id_lists(friend_table)
-    # print(nickname_table)
-    # print(id_table)
-    operate(friend_table, nickname_table, id_table)
+    friend = SKE(filename)
+    print(friend.friend_list)
+
+
+    # friend_table = read_file(filename)
+    # # print(friend_table)
+    # nickname_table = extract_nickname_lists(friend_table)
+    # id_table = extract_id_lists(friend_table)
+    # # print(nickname_table)
+    # # print(id_table)
+    # operate(friend_table, nickname_table, id_table)
+
 
 if __name__ == '__main__':
     doctest.testmod()
