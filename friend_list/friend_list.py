9269787e7fdd6c3a3f5c8b57b2c92d2b56cf1b99
Godiez1 <Godjangg@gmail.com>
2021-09-12 10:39:58 +0700

    I have just use some decorator and update little bit.

--- friend_list/friend_list.py
+++ friend_list/friend_list.py
@@ -1,11 +1,11 @@
-import random, sys, doctest
+import random, doctest
+
 
 class SKE:
     def __init__(self, filename):
         self.friend_table = self.read_friend(filename)
-        self.nickname_table = self.extract_nickname_list()
-        self.id_table = self.extract_id_lists()
-
+        self.nickname_table = self.extract_nickname_list
+        self.id_table = self.extract_id_lists
 
     @staticmethod
     def read_friend(filename):
@@ -13,31 +13,30 @@ class SKE:
         friend_list = [i.split() for i in lines]
         return friend_list
 
-
+    @property
     def extract_nickname_list(self):
         return [self.friend_table[i][3] for i in range(len(self.friend_table))]
 
-
+    @property
     def extract_id_lists(self):
         return [self.friend_table[i][0] for i in range(len(self.friend_table))]
 
-
-    def read_nickname(self, member):
+    @staticmethod
+    def read_nickname(member):
         list_nickname = []
         for i in range(member):
             list_nickname.append(input(f"Enter Nickname member{i + 1}: "))
         return list_nickname
 
-
-    def find_index(self, table, list_input):
+    @staticmethod
+    def find_index(table, list_input):
         list_nickname_index = []
         for i in list_input:
             for j in table:
                 if i.lower() == j.lower() and table.index(j) not in list_nickname_index:
                     list_nickname_index.append(table.index(j))
-                    break
-        return sorted(list_nickname_index)
 
+        return sorted(list_nickname_index)
 
     def random_member(self, list_nickname_index, member):
         random_time = int(input("How many people: "))
@@ -52,11 +51,9 @@ class SKE:
         for i in range(len(random_member_index)):
             print(f"{i + 1}:", *self.friend_table[list_nickname_index[random_member_index[i]]])
 
-
     def display_members(self, list_nickname_index):
         for i in list_nickname_index:
             print(*self.friend_table[i])
-
 
     def group_members(self):
         member = int(input("How Many people in a Group: "))
@@ -66,7 +63,6 @@ class SKE:
         self.display_members(list_nickname_index)
         if input("Random members? y/n: ").lower() == "y":
             self.random_member(list_nickname_index, member)
-
 
     def operate(self):
         print("1. Group")
@@ -86,10 +82,12 @@ class SKE:
                 id_student = [input("Enter ID: ")]
                 self.display_members(self.find_index(self.id_table, id_student))
 
+
 def main():
     filename = "friend_list.txt"
     friend = SKE(filename)
     friend.operate()
+
 
 if __name__ == '__main__':
     doctest.testmod()
