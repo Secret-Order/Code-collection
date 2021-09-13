994674069a95a2432fafb7189ad31bcc6e85433d
Godiez1 <80502090+Godiez1@users.noreply.github.com>
2021-09-13 23:27:18 +0700

    Want doc string from ryu (#3)
    * Merge Request: Class-based (#2)
    
    * Update #1
    
    * Unfinished yet
    
    * unfinished #3
    
    * Update #4
    
    * update #finished but maybe redundant #1
    
    * I have just use some decorator and update little bit.
    
    - Edited and removed unused import, fixed imports and fixed inconsistent blank lines (PEP-8).
    
    Co-authored-by: @Godiez1
    
    * Delete .DS_Store
    
    * update docstring
    
    Co-authored-by: Pawitchaya Chaloeijanya <39559733+GToidZ@users.noreply.github.com>
    Co-authored-by: PakornL <48524886+PakornL@users.noreply.github.com>

--- friend_list/friend_list.py
+++ friend_list/friend_list.py
@@ -10,6 +10,12 @@ class SKE:
 
     @staticmethod
     def read_friend(filename):
+        """
+        Read file and convert to list
+        :param filename:
+        :return: list of friend information
+        """
+
         lines = open(filename, encoding="utf8").read().splitlines()
         friend_list = [i.split() for i in lines]
         return friend_list
@@ -17,16 +23,32 @@ class SKE:
 
     @property
     def extract_nickname_list(self):
+        """
+        Extract friend_table to nickname list
+        :return: list of friend nickname
+        """
+
         return [self.friend_table[i][3] for i in range(len(self.friend_table))]
 
 
     @property
     def extract_id_lists(self):
+        """
+        Extract friend_table to KU ID list
+        :return: list of friend KU ID
+        """
+
         return [self.friend_table[i][0] for i in range(len(self.friend_table))]
 
 
     @staticmethod
     def read_nickname(member):
+        """
+        Enter friend nickname to list of nickname in group
+        :param member: number of members
+        :return: list of nickname of member in group
+        """
+
         list_nickname = []
         for i in range(member):
             list_nickname.append(input(f"Enter Nickname member{i + 1}: "))
@@ -35,6 +57,28 @@ class SKE:
 
     @staticmethod
     def find_index(table, list_input):
+        """
+        Find index of friend_table and sort it
+        :param table: list of friend information (such as nickname_table, id_table)
+        :param list_input: list of information (such as nickname, id)
+        :return: index of friend_table that match the list_input
+        """
+
+        table_index = []
+        for i in list_input:
+            for j in table:
+                if i.lower() == j.lower() and table.index(j) not in table_index:
+                    table_index.append(table.index(j))
+        return sorted(table_index)
+
+
+    def random_member(self, list_nickname_index, member):
+        """
+        Random members in group
+        :param list_nickname_index: list of index of all member in group
+        :param member: number of member
+        """
+
         list_nickname_index = []
         for i in list_input:
             for j in table:
@@ -44,33 +88,28 @@ class SKE:
         return sorted(list_nickname_index)
 
 
-    def random_member(self, list_nickname_index, member):
-        random_time = int(input("How many people: "))
-        random_member_index = []
-        while random_time > 0:
-            random_index = randint(0, member - 1)
-            if random_index in random_member_index:
-                pass
-            else:
-                random_member_index.append(random_index)
-                random_time -= 1
-        for i in range(len(random_member_index)):
-            print(f"{i + 1}:", *self.friend_table[list_nickname_index[random_member_index[i]]])
-
-
     def display_members(self, list_nickname_index):
+        """
+        Display all member in group
+        :param list_nickname_index: list of index of all member in group
+        """
+
         for i in list_nickname_index:
             print(*self.friend_table[i])
 
 
     def group_members(self):
+        """
+        input number of member and print it
+        :return: number of member and list of index of all member in group
+        """
+
         member = int(input("How Many people in a Group: "))
         list_nickname = self.read_nickname(member)
         list_nickname_index = self.find_index(self.nickname_table, list_nickname)
         print("Group members")
         self.display_members(list_nickname_index)
-        if input("Random members? y/n: ").lower() == "y":
-            self.random_member(list_nickname_index, member)
+        return member, list_nickname_index
 
 
     def operate(self):
@@ -78,16 +117,22 @@ class SKE:
         print("2. Person")
         choice_1 = int(input("Enter choice: "))
         if choice_1 == 1:
-            self.group_members()
+            member, list_nickname_index = self.group_members()
+            print("1. Random members")
+            print("2. Random work")
+            choice_2 = int(input("Enter choice: "))
+            if choice_2 == 1:
+                self.random_member(list_nickname_index, member)
+            # Random work currently updating
         elif choice_1 == 2:
             print("What do you have")
             print("1. Nickname")
             print("2. ID")
-            choice_2 = int(input("Enter choice: "))
-            if choice_2 == 1:
+            choice_3 = int(input("Enter choice: "))
+            if choice_3 == 1:
                 nickname = [input("Enter Nickname: ")]
                 self.display_members(self.find_index(self.nickname_table, nickname))
-            elif choice_2 == 2:
+            elif choice_3 == 2:
                 id_student = [input("Enter ID: ")]
                 self.display_members(self.find_index(self.id_table, id_student))
 
